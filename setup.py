1a9c3412edb63135c5abf48d69bec95a493d36c3
tjtharrison <tjtharrison@users.noreply.github.com>
2023-04-10 19:20:55 +0000

    Linted and formatted files

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 with open("README.md", encoding="UTF-8") as readme_file:
     readme_contents = readme_file.read()
 
-with open('requirements.txt', encoding="UTF-8") as requirements_file:
+with open("requirements.txt", encoding="UTF-8") as requirements_file:
     required = requirements_file.read().splitlines()
 
 __version__ = "1.1.1"
@@ -17,5 +17,5 @@ setup(
     long_description=readme_contents,
     long_description_content_type="text/markdown",
     install_requires=required,
-    scripts=['gpush']
+    scripts=["gpush"],
 )
