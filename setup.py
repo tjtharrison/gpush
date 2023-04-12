263d8faf72a279141eb21853b869547bb7993af8
tjtharrison <tjtharrison@users.noreply.github.com>
2023-04-12 19:29:03 +0000

    Linted and formatted files

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 Setup file for python-semantic-versioning
 """
 from setuptools import setup
+
 from _version import __version__
 
 with open("README.md", encoding="UTF-8") as readme_file:
@@ -16,5 +17,5 @@ setup(
     long_description=readme_contents,
     long_description_content_type="text/markdown",
     install_requires=required,
-    scripts=["gpush", "_version.py"]
+    scripts=["gpush", "_version.py"],
 )
