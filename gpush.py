98dae03f6fdbe1369eb17200d087f3186f777657
tjtharrison <gh@tjth.co>
2023-06-10 19:19:32 +0100

    Improved error handling

--- gpush.py
+++ gpush.py
@@ -1,20 +1,25 @@
 #!/usr/bin/env python3
 
 """
-Python script to handle git commit and push to standardise commit messages using conventional commit
-messages.
+Python script to handle git commit and push to standardise commit messages using conventional commit messages.
 
 Usage: gpush.py
 """
 
 import argparse
-
+import logging
+import sys
 import inquirer
-from git import Repo
+from git import Repo, exc
 
 from _version import __version__
 
 VERSION = __version__
+
+# Setup logging
+
+
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 parser = argparse.ArgumentParser(
     prog="gpush " + VERSION,
@@ -51,29 +56,36 @@ args = parser.parse_args()
 
 def get_version():
     """
-    Function to return the current version of gpush.py
+    Return the current version of gpush.py.
 
-    :return: String containing the current version of gpush.py
+    Returns:
+        String: Current version of gpush.py
     """
     return VERSION
 
 
 def git_commit(commit_message):
     """
-    Function to commit changes to Git on the current branch for the repository
-    :param commit_message: String containing the conventional commit message formatted commit
-    message
-    :return: True/False
+    Commit changes to Git on the current branch for the repository.
+
+    Args:
+        commit_message: String containing the conventional commit message formatted commit message
+
+    Raises:
+        exc.GitError: If there is an issue with the call to git
+
+    Returns:
+        True if commit is successful
     """
     try:
         repo = Repo(search_parent_directories=True)
         repo.index.write()
         repo.git.commit("-m" + commit_message)
-        print("committing: " + commit_message)
-        print("Committed successfully")
-    except Exception as error_message:
-        print("Some error occured while committing the code:")
-        print(str(error_message))
+        logging.info("committing: " + commit_message)
+        logging.info("Committed successfully")
+    except exc.GitError as error_message:
+        logging.info("Some error occured while committing the code:")
+        logging.info(str(error_message))
         raise
 
     return True
@@ -81,9 +93,14 @@ def git_commit(commit_message):
 
 def git_push():
     """
-    Function to push commit up to Git on the current branch for the repository
+    Push commit up to Git on the current branch for the repository.
 
-    :return: True/False
+    Raises:
+        GitError: If there is an issue with the call to git
+        Exception: If there is some other issue
+
+    Returns:
+        True if push is successful
     """
     try:
         if str(args.branch) != "current":
@@ -94,37 +111,52 @@ def git_push():
         repo = Repo(search_parent_directories=True)
         repo.create_head(branch_name)
         repo.git.push("--set-upstream", "origin", branch_name)
-        print("Pushed successfully")
+        logging.info("Pushed successfully")
+    except exc.GitError as error_message:
+        logging.info("Some error occurred while pushing the code:")
+        logging.info(str(error_message))
+        raise exc.GitError
     except Exception as error_message:
-        print("Some error occurred while pushing the code:")
-        print(str(error_message))
-        raise
+        logging.info("Some error occurred while pushing the code:")
+        logging.info(str(error_message))
+        raise Exception
 
     return True
 
 
 def collect_details():
     """
-    Function that collects commit message detail from the committer and executes git commit
-    and push.
+    Collect commit message detail from the committer and executes git commit and push.
 
-    :return: True/False
+    Raises:
+        KeyboardInterrupt: If user exits the script
+
+    Returns:
+        commit_message_final: String containing the conventional commit message formatted commit message
     """
-    questions = [
-        inquirer.List(
-            "type",
-            message="What type of commit is this?",
-            choices=["fix", "feat", "docs", "ci"],
-        ),
-        inquirer.List(
-            "breaking_change",
-            message="Does the commit include breaking changes?",
-            choices=["Yes", "No"],
-        ),
-        inquirer.Text("commit_message", message="What's your commit message"),
-    ]
+    try:
+        questions = [
+            inquirer.List(
+                "type",
+                message="What type of commit is this?",
+                choices=["fix", "feat", "docs", "ci"],
+            ),
+            inquirer.List(
+                "breaking_change",
+                message="Does the commit include breaking changes?",
+                choices=["Yes", "No"],
+            ),
+            inquirer.Text(
+                "commit_message",
+                message="What's your commit message",
+            ),
+        ]
+        answers = inquirer.prompt(
+            questions,
+            raise_keyboard_interrupt=True)
+    except KeyboardInterrupt as error_message:
+        raise KeyboardInterrupt
 
-    answers = inquirer.prompt(questions)
     if answers["breaking_change"] == "Yes":
         is_breaking_change = "!"
     else:
@@ -133,16 +165,19 @@ def collect_details():
     commit_message_final = (
         answers["type"] + is_breaking_change + ": " + answers["commit_message"]
     )
+
     return commit_message_final
 
 
 def main():
     """
-    Main function to execute the script
-    :return:
+    Execute the script.
+
+    Returns:
+        True
     """
     if args.version:
-        print(get_version())
+        logging.info(get_version())
     else:
         try:
             if args.no_commit:
@@ -154,9 +189,12 @@ def main():
             if args.no_push:
                 git_push()
         except Exception as error_message:
-            print("Some error occurred while pushing the code:")
-            print(str(error_message))
-            raise
+            logging.info("Some error occurred while pushing the code:")
+            logging.info(str(error_message))
+            sys.exit(1)
+        except KeyboardInterrupt as error_message:
+            logging.info("Okay! Bye!")
+            sys.exit(0)
     return True
 
 
